d625fd2383e214d680b95b2dcf76f3380969deda
MabdoulYoussef <mabdoulyoussef@hotmail.com>
2025-02-28 23:05:24 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 export default function () {
     return (
         <div>
-
+            <h1>Team</h1>
+            <p>Here is a list of our team members:</p>
+            <ul>
+                <li>Player 1</li>
+                <li>Player 2</li>
+                <li>Player 3</li>
+            </ul>
         </div>
     )
 }
