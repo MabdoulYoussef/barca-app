e4f9d06acdf341cbd5c4be83229b3fa0af6573a9
MabdoulYoussef <mabdoulyoussef@hotmail.com>
2025-03-11 00:28:47 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,20 @@ const Navbar = () => {
                 <Link to="/matches">Matches</Link>
                 <Link to="/store">Store</Link>
             </div>
+            <p>
+                <a href="https://www.facebook.com/fcbbarcelona" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-facebook-square"></i>
+                </a>
+                <a href="https://www.instagram.com/fcbbarcelona/" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-instagram"></i>
+                </a>
+                <a href="https://twitter.com/fcbbarcelona" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-twitter"></i>
+                </a>
+                <a href="https://www.youtube.com/user/FCBarcelona" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-youtube"></i>
+                </a>
+            </p>
         </nav>
     );
 };
